4661d6d7f9529d3279cba374dada05f9e3a84be5
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-17 09:58:30 +0200

    Programma klaar

---
+++ Priemgetallen/Priemgetallen/Program.cs
@@ -0,0 +1,30 @@
+﻿for (int number = 1; number <= 1000000; number++)
+{
+    bool isPrime = true;
+
+    for (int check = 2; check <= number / 2; check++)
+    {
+        if (number % check == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+
+    Console.Write($"{number}\t");
+    Console.ResetColor();
+
+    if (number % 10 == 0)
+    {
+        Console.WriteLine();
+    }
+}
